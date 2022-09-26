a32985a6cfc448f70f98f47fe5b5d9f4b9406261
Austin1a2b <cavalier283@gmail.com>
2022-09-26 10:53:17 +0800

    進行部屬

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/alpaca-image-generator/'           //這邊是 repo 專案名稱
+    : '/'                          //根目錄
 })
+
+vue.config.js
